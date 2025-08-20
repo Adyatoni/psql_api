aad735a3382bb703baec4664e933e4b61960cd17
Adyatoni <adyatonibiswas1002@gmail.com>
2025-08-20 21:09:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// index.js
+const express = require("express");
+const usersRouter = require("./routes/users");
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+// Routes
+app.use("/users", usersRouter);
+
+// Root
+app.get("/", (req, res) => {
+  res.send("Postgres + Node.js API is running 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
