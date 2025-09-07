eb86e01d54a0b084bd4e49e9cb35bbf0cf14cdb2
Adyatoni <adyatonibiswas1002@gmail.com>
2025-09-07 11:52:16 +0530

    feature : add redis

--- routes/getusers.ts
+++ routes/getusers.ts
@@ -1,18 +1,27 @@
 import { Router, Request, Response } from "express";
 import User from "../models/usermodel.js";
 import { verifyToken, auth_req } from "../middleware/authmw";
+import redis from "../config/redis";
 
 
 const router = Router();
 
 router.get("/", verifyToken, async (_req: auth_req, res: Response) => {
   try {
+      const cacheData = await redis.get("users");
+      if (cacheData) {
+      console.log("data from cache");
+      return res.json(JSON.parse(cacheData));
+      }
+
+    console.log("data from db");
     const users = await User.findAll();
+    await redis.set("users", JSON.stringify(users), "EX", 60);
     res.json(users);
-  } catch (err : any) {
-    res.status(500).json({ error: err.message });
-  }
-});
+   } catch (err : any) {
+     res.status(500).json({ error: err.message });
+   }
+ });
 
 // update email feature
 router.put("/email/:id", async (req: Request, res: Response) => {
@@ -31,6 +40,8 @@ router.put("/email/:id", async (req: Request, res: Response) => {
 
     u.email = email;
     await u.save();
+    await redis.del("users");
+    console.log("cache data deleted");
 
     res.json({ msg: "email updated", email: u.email });
   } catch (err) {
