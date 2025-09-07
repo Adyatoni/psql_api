081069a575107e4a8bf8056c5711f2d68042e7dc
Adyatoni Biswas <126558390+Adyatoni@users.noreply.github.com>
2025-09-07 11:59:29 +0530

    Update redis.ts

--- config/redis.ts
+++ config/redis.ts
@@ -1,10 +1,8 @@
-// config/redis.ts
 import Redis from "ioredis";
 
 const redis = new Redis({
   host: "127.0.0.1",
   port: 6379,
-  // password: process.env.REDIS_PASS,  // if you add auth
 });
 
 export default redis;
